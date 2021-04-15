710b1dff7b7f985abef28dd65b62bcb067897ef9
Alexandre Spehler <alexandre.spehler@gmail.com>
2021-04-15 12:51:21 -0400

    update variable with cli

--- cart/lib/repo.js
+++ cart/lib/repo.js
@@ -1,6 +1,7 @@
 const CLI = require('clui');
 const fs = require('fs-extra')
 const simpleGit = require('simple-git');
+const replace = require('replace-in-file');
 const Spinner = CLI.Spinner;
 const touch = require('touch');
 const _ = require('lodash');
@@ -23,32 +24,51 @@ module.exports = {
     //   touch('test2.tmp');
     // }
 
-    const srcDir = '../boilerplates/meteor/meteor-react/';
+    const srcDir = '../boilerplates/meteor/skel-react/';
     const destDir = '../src/';
+
+    const status = new Spinner('Generating Boilerplate...');
+    status.start();
 
     // Delete folder
     await fs.remove(destDir)
       .then(() => {
-        console.log('removed!');
+        console.log('Previous version removed!');
       })
       .catch(err => {
         console.error(err)
-      })
+      });
 
     // create folder
     await fs.copy(srcDir, destDir).then(() => {
-        console.log('done!');
+        console.log('Boilerplate generated!');
+        status.stop();
       })
       .catch(err => {
         console.error(err)
+      });
+
+    const METEOR_RELEASE = 'METEOR@2.1';
+
+    const options = {
+        files: "../src/.meteor/*",
+        from: [/{{! METEOR_RELEASE !}}/g],
+        to: [METEOR_RELEASE]
+    };
+
+    replace(options)
+      .then(result => {
+          console.log("Replacement results: ",result);
       })
+      .catch(error => {
+          console.log(error);
+      });
   },
 
   setupRepo: async () => {
     const status = new Spinner('Test2 git commands...');
     const git = simpleGit();
     status.start();
-
     try {
       // git.add('test2.tmp')
         // .then(git.commit('Initial commit'))
